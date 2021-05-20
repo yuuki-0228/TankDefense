5f4c15f43117c49ff8bcb806dc712067d0e67bce
reiya <reiya415@icloud.com>
2021-05-20 11:24:56 +0900

    当たり判定初期化修正

--- TankDefense/SourceCode/Object/Collider/Colliders/Colliders.cpp
+++ TankDefense/SourceCode/Object/Collider/Colliders/Colliders.cpp
@@ -12,7 +12,9 @@ CCollisions::~CCollisions()
 {
 }
 
+//-----------------------------------------.
 // éwíËÇµÇΩìñÇΩÇËîªíËÇÃèâä˙âª.
+//-----------------------------------------.
 void CCollisions::InitCollision( const ECollNo& no, const STranceform& t )
 {
 	switch( no )
@@ -20,12 +22,15 @@ void CCollisions::InitCollision( const ECollNo& no, const STranceform& t )
 	case ECollNo::Sphere:
 		m_pSphere = std::make_unique<CSphere>();
 		m_pSphere->SetTranceform( t );
+		break;
 	case ECollNo::Capsule:
 		m_pCapsule = std::make_unique<CCapsule>();
 		m_pCapsule->SetTranceform( t );
+		break;
 	case ECollNo::Box:
 		m_pBox = std::make_unique<CBox>();
 		m_pBox->SetTranceform( t );
+		break;
 	case ECollNo::Ray:
 		m_pRay = std::make_unique<CRay>();
 		m_pRay->SetTranceform( t );
@@ -33,6 +38,7 @@ void CCollisions::InitCollision( const ECollNo& no, const STranceform& t )
 	case ECollNo::Mesh:
 		m_pMesh = std::make_unique<CMesh>();
 		m_pMesh->SetTranceform( t );
+		break;
 	default:
 		break;
 	}
